4c95c7ac1356ee9895a3db15ed8d7b69c2a7894a
ofiallos27163-rgb <ofiallos27163@columbushs.com>
2025-10-02 08:47:28 -0400

    finished q1 and q2

--- src/Lab1_12.java
+++ src/Lab1_12.java
@@ -1,4 +1,4 @@
-
+import java.awt.Point;
 public class Lab1_12 {
     public static void main(String[] args) {
         System.out.println("Task 1: " + q1());
@@ -10,18 +10,25 @@ public class Lab1_12 {
     // 1. Create Point (5,10) and return coordinates
     public static String q1() {
         // TODO
-        return "";
+        Point p1 = new Point(5, 10);
+        String result1 = p1.getX() + ", " + p1.getY();
+        return result1;
     }
 
     // 2. Move a Point (0,0) to (4,7) and return coordinates
     public static String q2() {
         // TODO
-        return "";
+        Point p1 = new Point();
+        p1.move(4,7);
+        String result1 = p1.getX() + ", " + p1.getY();
+        return result1;
     }
 
     // 3. Find distance between (0,0) and (3,4)
     public static double q3() {
         // TODO
+        Point p1 = new Point();
+        Point p2 = new Point(3, 4);
         return 0.0;
     }
 
